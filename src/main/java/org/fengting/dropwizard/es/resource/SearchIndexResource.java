0a34930f28f35dceb778e863aa95b75ea1dc2c04
fengtingzhang <fengtingzhang@gmail.com>
2015-05-31 15:16:19 -0400

    Use resource sample data array

--- src/main/java/org/fengting/dropwizard/es/resource/SearchIndexResource.java
+++ src/main/java/org/fengting/dropwizard/es/resource/SearchIndexResource.java
@@ -4,15 +4,19 @@ import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.action.index.IndexResponse;
-import org.fengting.dropwizard.es.api.Tweet;
 import org.fengting.dropwizard.es.manager.SearchManager;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Date;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import org.json.JSONArray;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
@@ -30,34 +34,50 @@ public class SearchIndexResource {
     @GET
     @Timed
     public String addTweets() {
-        //String json = getTestDataFromString();
-        byte[] json = getTestDataFromJackson(new Tweet("kimchy", new Date(), "trying out ElasticSearch"));
-        IndexResponse response = manager.obtainClient().prepareIndex("twitter", "tweet")
-                .setSource(json)
-                .execute()
-                .actionGet();
-
+        //byte[] json = getTestDataFromJackson(new Tweet("kimchy", new Date(), "trying out ElasticSearch"));
+        JSONArray indexResults = new JSONArray();
         try {
-            return jsonBuilder()
-                    .startObject()
-                    .field("index", response.getIndex())
-                    .field("type", response.getType())
-                    .field("id", response.getId())
-                    .field("version", response.getVersion())
-                    .field("created", response.isCreated())
-                    .endObject().string();
+            JSONArray data = getTestDataFromString();
+            for (int i = 0; i < data.length(); i++) {
+                IndexResponse response = manager.obtainClient().prepareIndex("twitter", "tweet")
+                        .setSource(data.get(i).toString())
+                        .execute()
+                        .actionGet();
+                indexResults.put(jsonBuilder()
+                        .startObject()
+                        .field("index", response.getIndex())
+                        .field("type", response.getType())
+                        .field("id", response.getId())
+                        .field("version", response.getVersion())
+                        .field("created", response.isCreated())
+                        .endObject().string());
+            }
         } catch (IOException e) {
             return e.toString();
         }
+
+        return indexResults.toString();
     }
 
-    private String getTestDataFromString() {
-        return "{" +
-                "\"user\":\"kimchy\"," +
-                "\"postDate\":\"2013-01-30\"," +
-                "\"another\":\"anoterfield\"," +
-                "\"message\":\"trying out Elasticsearch\"" +
-                "}";
+    private JSONArray getTestDataFromString() throws IOException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("data.json");
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        StringBuilder responseStrBuilder = new StringBuilder();
+
+        String inputStr;
+        while ((inputStr = streamReader.readLine()) != null) {
+            responseStrBuilder.append(inputStr);
+        }
+        JSONArray results = new JSONArray(responseStrBuilder.toString());
+        return results;
+
+//
+//        return "{" +
+//                "\"user\":\"kimchy\"," +
+//                "\"postDate\":\"2013-01-30\"," +
+//                "\"another\":\"anoterfield\"," +
+//                "\"message\":\"trying out Elasticsearch\"" +
+//                "}";
     }
 
     private byte[] getTestDataFromJackson(Object value) {
